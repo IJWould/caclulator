ed592e27c80cdcbdf9e8ded809b0e47cc8c6bb74
Alexander <alexander.al3ksand3r@yandex.ru>
2024-11-18 17:49:45 +0300

    v-2

--- script.js
+++ script.js
@@ -10,86 +10,188 @@ let parentheses = '';
 
 let inputCopy = '';
 
-let operands = {
-    lNum: 0,
-    rNum: 0,
-    expression(operand, lnum, rnum) {
-        this.lNum = Number(lnum);
-        this.rNum = Number(rnum);
+function expression(operator='+', lnum=0, rnum=0) {
 
-        switch (operand) {
-            case '+':
-                return this.lNum + this.rNum;
+    let lNum = parseFloat(lnum);
+    let rNum = parseFloat(rnum);
 
-            case '-':
-
-                return this.lNum - this.rNum;
-            case '*':
-                return this.lNum * this.rNum;
-            case '/':
-                if (this.rNum !== 0) {
-                    return this.lNum / this.rNum;
-                } else {
-                    input.value = 'Ошибка';
-                }
-            default:
-                input.value = 'Ошибка';
-        }
-    }
-};
-
-function priority(string){
-
-    let priorityList = [];
-
-    for (let i = 0; i < string.length; i++){
-
-        switch(string[i]){
-            case ')':
-            case '(':
-                priorityList.push([1, i])
-                break;
-            case '*':
-            case '/':
-                priorityList.push([2, i])
-                break;
-            case '+':
-            case '-':
-                priorityList.push([3, i])
-                break;
-        }
-
+    if (typeof lnum === 'string' && lnum.includes('%')){
+        lNum /= 100;
+   }
+    if (typeof rnum === 'string' && rnum.includes('%')){
+        rNum = lNum / 100 * rNum;
 
     };
 
+
+    switch (operator) {
+        case '+':
+            return lNum + rNum;
+        case '-':
+            return lNum - rNum;
+        case '*':
+            return lNum * rNum;
+        case '/':
+            return rNum !== 0 ? lNum / rNum : input.value = 'Ошибка';
+        default:
+            input.value = 'Ошибка';
+        };
+};
+
+
+
+
+function createPriorityList(string){
+
+    let priorityList = [];
+    let check = false;
+    for (let i = 0; i < string.length; i++){
+
+        if (string[i].includes('(')) {
+            check = true;
+            string[i] = string[i].slice(1);
+        }
+        else if (string[i].includes(')')){
+            check = false;
+            string[i] = string[i].slice(0, -1);
+        };
+        switch(string[i]){
+            case '*':
+            case '/':
+                check ? priorityList.push([0, i]) : priorityList.push([2, i]);
+                break;
+            case '+':
+            case '-':
+                check ? priorityList.push([1, i]) : priorityList.push([3, i]);
+                break;
+            default:
+                break;
+
+            };
+        };
     return priorityList.sort((a, b) => a[0] - b[0]);
+};
+
+function inputResult(){
+
+    const start = performance.now();
+
+    if (input.value.includes('(') && parentheses == '('){
+
+        input.value += ')';
+        inputCopy += ' )';
+        parentheses = '';
+    }
+
+
+    let res = inputCopy.split(' ');
+
+    let sortPriority = createPriorityList(res);
+
+    let operator;
+    let totalShift = 2;
+    let leftOperand =  -1;
+    let rightOperand = 1;
+    let shiftForParentheses = 2;
+
+    for (let i = 0; i < sortPriority.length; i++){
+        // if (sortPriority[i][0] == 0){
+        //     shiftForParentheses -= 2;
+
+        // }
+        // else if (sortPriority[i][0] == 1){
+        //     shiftForParentheses = -sortPriority.filter(elem =>elem[0] < 2 && elem[1] < sortPriority[i][1]).length * 2
+
+
+        if (sortPriority[i][0] == 2){
+
+           totalShift -= 2;
+
+        }
+        else if (sortPriority[i][0] == 3){
+
+            totalShift = -sortPriority.filter(elem =>elem[1] < sortPriority[i][1]).length * 2
+
+        };
+
+
+
+        operator = sortPriority[i][1] + totalShift
+
+
+        let dig = expression(res[operator], res[operator + leftOperand],
+            res[operator + rightOperand]);
+
+            console.log('operator:',sortPriority[i][0])
+
+            console.log('номер элемента:', i)
+
+            console.log('индекс выражения:',sortPriority[i][1])
+            console.log(...sortPriority)
+            console.log(res)
+            console.log('индекс левого операнда:', leftOperand)
+            console.log('индекс правого операнда:', rightOperand)
+            console.log('смещение:', totalShift)
+            console.log('смещенный индекс левого операнда:',operator + leftOperand)
+            console.log('смещенный индекс:', operator)
+            console.log('смещенный индекс правого операнда:',operator+ rightOperand)
+
+            console.log('----------------------------------------------------')
+
+
+        res[operator - 1] = dig;
+        res.splice(operator, 2)
+
+        console.log(res)
+        console.log('----------------------------------------------------')
+
+        }
+
+    const end = performance.now();
+    console.log(`Время выполнения: ${end - start} мс.`);
+    input.value = res;
+    inputCopy = res;
 }
-
-
 
 
 buttons.addEventListener('click', function(event){
     let button = event.target;
-
     if (button.tagName != 'BUTTON') return;
-
     calc(button.innerHTML)
-
 });
+
+
+
+input.addEventListener('keydown', function(event){
+    if (/[0-9%\/*\-+\(\)=]|Backspace|Enter/.test(event.key)){
+        calc(event.key);
+        event.preventDefault();
+    }
+});
+
+// document.addEventListener('keydown', event => {
+
+//     if ((event.key).match(/[0-9%\/*\-+\(\)=]|Backspace|Enter/)) calc(event.key)
+
+// })
 
 
 
 
 function calc(butt){
+
     switch(butt){
+        case 'Backspace':
         case 'DEL':
-            input.value = input.value.slice(0, input.value.length - 1)
-            inputCopy = inputCopy.slice(0, inputCopy.length - 1)
+            input.value = input.value.slice(0, - 1)
+            inputCopy = inputCopy.slice(0, - 1)
             break;
+
 
         case 'C':
             input.value = '';
             inputCopy = '';
+            parentheses ='';
             break;
 
         case '*':
@@ -99,108 +201,28 @@ function calc(butt){
             input.value += butt
             inputCopy += ' ' + butt + ' ';
             break;
+        case '%':
+            input.value += butt;
+            inputCopy += butt;
+            break;
 
         case '()':
             if (parentheses == '('){
                 parentheses = '';
                 input.value += ')';
-                inputCopy += ' ) ';
+                inputCopy += ')';
             }
 
             else{
-
                 input.value += '(';
-                inputCopy += ' ( ';
+                inputCopy += '(';
                 parentheses = '(';
-
             }
             break;
 
-
+        case 'Enter':
         case '=':
-
-            if (input.value.includes('(') && parentheses == '('){
-
-                input.value += ')';
-                inputCopy += ' ) '
-                parentheses = '';
-            }
-
-
-            let res = inputCopy.split(' ');
-
-            let sortPriority = priority(res);
-
-
-            let totalShift = 2;
-            let leftOperand =  -1;
-            let rightOperand = 1;
-            let indexPriority = sortPriority[0][0];
-
-
-
-
-
-
-            for (let i = 0; i < sortPriority.length; i++){
-
-                if (sortPriority[i][0] == 3){
-
-                    totalShift = -sortPriority.filter(elem => elem[1] < sortPriority[i][1]).length * 2
-
-                }
-
-                if (sortPriority[i][0] == 2){
-
-                    totalShift -= 2;
-                }
-
-                if (sortPriority[i][0] == 1){
-                    
-                }
-
-
-                let operator = sortPriority[i][1] + totalShift
-
-
-
-
-
-
-                let dig = operands.expression(res[operator], res[operator + leftOperand],
-                    res[operator + rightOperand]);
-
-                    console.log('indexPriority:', indexPriority)
-                    console.log('operator:',sortPriority[i][0])
-
-                    console.log('номер элемента:', i)
-
-                    console.log('индекс выражения:',operator)
-                    console.log(...sortPriority)
-                    console.log('индекс левого операнда:', leftOperand)
-                    console.log('индекс правого операнда:', rightOperand)
-                    console.log('смещение:', totalShift)
-                    console.log('смещенный индекс левого операнда:',operator + leftOperand)
-                    console.log('смещенный индекс:', operator - totalShift)
-                    console.log('смещенный индекс правого операнда:',operator+ rightOperand)
-
-                    console.log('----------------------------------------------------')
-
-
-                    res[operator - 1] = dig;
-                    res.splice(operator, 2)
-
-
-
-
-
-
-                    console.log(res)
-
-            }
-
-            input.value = res;
-
+            inputResult()
             break;
 
         default:
@@ -415,4 +437,3 @@ function calc(butt){
 //     //             console.log(arr)
 //     //             break;
 //     //     };
-//     // }
